1db3e9ef4b8afa443bb8f5b0fbb63eb24ad35095
Anton Chumakov <anton@chumakoff.com>
2025-01-09 13:04:37 +0300

    Init project

---
+++ src/main/java/com/chumakoff/mealvoting/MealVotingApplication.java
@@ -0,0 +1,13 @@
+package com.chumakoff.mealvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chumakoff/mealvoting/MealVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chumakoff.mealvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
